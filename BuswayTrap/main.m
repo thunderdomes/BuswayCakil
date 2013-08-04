15d21a89f1bc7b4a181ddfb12140768700b758f1
Arie <arie@wego.com>
2013-08-04 15:00:33 +0700

    Initial Commit

---
+++ BuswayTrap/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BuswayTrap
+//
+//  Created by Arie on 8/4/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "netraAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([netraAppDelegate class]));
+	}
+}
---
+++ BuswayTrap/netraAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  netraAppDelegate.h
+//  BuswayTrap
+//
+//  Created by Arie on 8/4/13.
+//  Copyright (c) 2013 netra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface netraAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
